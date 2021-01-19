38566e046738ef9cd90f7024de98a9cf00cb42cf
sergey <remote.c.dzencode@gmail.com>
2021-01-19 09:52:19 +0200

    basic test done

--- tests/BasicsTest.php
+++ tests/BasicsTest.php
@@ -17,12 +17,15 @@ class BasicsTest extends TestCase
 
         // The function name, or {closure} for anonymous functions.
         // TODO __CLASS__
+        $this->assertEquals('BasicsTest', __CLASS__);
 
         // The class method name.
         // TODO __METHOD__
+        $this->assertEquals('BasicsTest::testMagicConstants', __METHOD__);
 
         // The name of the current namespace.
         // TODO __NAMESPACE__
+        $this->assertEquals('', __NAMESPACE__);
     }
 
     /**
@@ -36,20 +39,28 @@ class BasicsTest extends TestCase
 
         // Negative integers
         // TODO (bool) -1
+        $this->assertEquals(true, (bool) -1);
         // TODO (bool) -10
+        $this->assertEquals(true, (bool) -10);
         // TODO (bool) 0
+        $this->assertEquals(false, (bool) 0);
 
         // Strings
         // TODO (bool) ''
+        $this->assertEquals(false, (bool) '');
         // TODO (bool) 'false'
+        $this->assertEquals(true, (bool) 'false');
         // TODO (bool) 'not empty string'
+        $this->assertEquals(true, (bool) 'not empty string');
 
         // Arrays
         // TODO (bool) []
+        $this->assertEquals(false, (bool) []);
         // TODO (bool) [1, 2, 3]
-
+        $this->assertEquals(true, (bool) [1, 2, 3]);
         // Null
         // TODO (bool) null
+        $this->assertEquals(false, (bool) null);
     }
 
     /**
@@ -65,15 +76,18 @@ class BasicsTest extends TestCase
 
         // Multiplication
         // TODO to be implemented
+        $this->assertEquals(10, 2 * 5);
 
         // Division
         // TODO to be implemented
+        $this->assertEquals(20, 100 / 5);
 
         // Modulo
         // TODO to be implemented
-
+        $this->assertEquals(1, 10 % 3);
         // Exponentiation
         // TODO to be implemented
+        $this->assertEquals(8, 2 ** 3);
     }
 
     /**
@@ -100,9 +114,15 @@ class BasicsTest extends TestCase
         $this->assertIsInt($foo);
 
         // TODO $foo = $foo * 1.3;
+        $foo *= 1.3;
+        $this->assertIsFloat($foo);
 
         // TODO $foo = 5 * (int) '10 Little Piggies';
+        $foo = 5 * (int) '10 Little Piggies';
+        $this->assertIsInt($foo);
 
         // TODO $foo = 5 * (int) '10 Small Pigs';
+        $foo = $foo = 5 * (int) '10 Small Pigs';
+        $this->assertIsInt($foo);
     }
 }
